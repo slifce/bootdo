a0a13356377b11ff6bf535fe5eb7bc05b51bb661
slifce <734507707@qq.com>
2022-04-14 23:50:07 +0800

    通过jmeter模拟高并发减库存操作

---
+++ src/main/java/com/bootdo/frequent/controller/ShopCartController.java
@@ -0,0 +1,35 @@
+package com.bootdo.frequent.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/shop")
+public class ShopCartController {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/submitOrder")
+    @ResponseBody
+    public String submitOrder() {
+        // 超卖情况使用加锁
+        synchronized (this) {
+            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+            if (stock > 0) {
+                stock = stock - 1; // 更新库存
+                stringRedisTemplate.opsForValue().set("stock", stock + "");
+                System.out.println("扣减成功，库存stock:" + stock);
+                Boolean setIfAbsent = stringRedisTemplate.opsForValue().setIfAbsent("stock", stock + "");
+            } else {
+                System.out.println("扣减失败，库存不足"); // 下单失败
+                return "error";
+            }
+        }
+        return "end";
+    }
+
+}
