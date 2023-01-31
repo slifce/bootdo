aa039d1ce24385131d5cc1f05747d8c35c28d747
Slifce <734507707@qq.com>
2023-01-31 20:57:31 +0800

    重构设计模式以及校验器

--- src/main/java/com/bootdo/common/validator/base/ValidatorBase.java
+++ src/main/java/com/bootdo/common/validator/base/ValidatorBase.java
@@ -23,41 +23,4 @@ public interface ValidatorBase {
      * false 不再继续后续校验
      */
     boolean continueValidate();
-
-    abstract class AbstractErrorMsgSignValidator implements ValidatorBase {
-        /**
-         * 错误信息
-         */
-        protected String errorMsg = null;
-
-        /**
-         * 真正的校验方法，转换一下返回类型
-         *
-         * @param dynamicObject 申请单实体
-         * @return 错误信息
-         */
-        protected abstract String realValidate(DynamicObject dynamicObject);
-
-        /**
-         * 校验，返回错误信息
-         *
-         * @param dynamicObject 申请实体
-         * @return 错误信息
-         */
-        @Override
-        public String validate(DynamicObject dynamicObject) {
-            return realValidate(dynamicObject);
-        }
-
-        /**
-         * 是否继续校验
-         *
-         * @return true 继续后续校验
-         * false 不再继续后续校验
-         */
-        @Override
-        public boolean continueValidate() {
-            return StringUtils.isEmpty(errorMsg);
-        }
-    }
 }
